9f4d944920fd51f1724066d6a000e6072ebc0115
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-07-17 23:16:05 +0100

    initializes repository, creates react vite app and installs tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { NavBar } from './components/NavBar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from './screens/Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/screens/Home.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../components/NavBar";
+
+export function Home() {
+
+    return (
+        <section>
+            <NavBar />
+            <h1 className="text-black">Project Portfolio</h1>
+        </section>
+    )
+}
